e5ea6014bf3cd4e3d1ff1b84ff0cefca12fc6596
caiwenjie@anosi.cn <caiwenjie@anosi.cn>
2024-06-21 17:35:38 +0800

    联调用户管理

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,16 +1,16 @@
-import { Injectable,HttpException, HttpStatus,Inject ,UnauthorizedException} from '@nestjs/common';
+import { Injectable, HttpException, HttpStatus, Inject, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import {  Repository } from 'typeorm';
+import { Repository } from 'typeorm';
 import { JwtService } from '@nestjs/jwt';
 import { TokenExpiredError } from 'jsonwebtoken'; // 确保导入TokenExpiredError
-import {User} from '@/user/entities/user.entity';
-import {Role} from '@/role/entities/role.entity';
-import {Menu} from '@/menu/entities/menu.entity';
+import { User } from '@/user/entities/user.entity';
+import { Role } from '@/role/entities/role.entity';
+import { Menu } from '@/menu/entities/menu.entity';
 import { LoginDto } from './dto/login.dto';
-import {LoginVo} from './vo/login.vo';
-import {md5} from '@/utils'
+import { LoginVo } from './vo/login.vo';
+import { md5 } from '@/utils'
 import { ConfigService } from '@nestjs/config';
-import {RedisService} from '@/redis/redis.service'
+import { RedisService } from '@/redis/redis.service'
 
 @Injectable()
 export class AuthService {
@@ -33,102 +33,105 @@ export class AuthService {
   @Inject(RedisService)
   private redisService: RedisService;
 
-  async haneleLogin(logindata: LoginDto){
+  async haneleLogin(logindata: LoginDto) {
+    const user = await this.userRepository.findOne({
+      where: {
+        username: logindata.username,
+      },
+      relations: ['roles']
+    });
+    if (!user) {
+      throw new HttpException('用户名或者密码错误', HttpStatus.BAD_REQUEST)
+    }
+    if (md5(logindata.password) !== user.password) {
+      throw new HttpException('用户名或者密码错误', HttpStatus.BAD_REQUEST)
+    }
+    if (user.accountStatus === false) {
+      throw new HttpException('账号已被禁用', HttpStatus.BAD_REQUEST)
+    }
+
+    const vo = new LoginVo();
+    vo.access_token = this.jwtService.sign({ ...user }, {
+      expiresIn: this.configService.get('jwt.expireIn') || '30m'
+    });
+    vo.refresh_token = this.jwtService.sign({ ...user }, {
+      expiresIn: this.configService.get('jwt.refreshExpire') || '7d'
+    });
+    const access_token_sessionid = vo.access_token.slice(-10)
+    const refresh_token_sessionid = vo.refresh_token.slice(-10)
+    this.redisService.setToken(`refreshToken:${refresh_token_sessionid}`, vo.refresh_token)
+    this.redisService.setToken(`accessToken:${access_token_sessionid}`, vo.access_token)
+    return vo
+  }
+
+  async refreshtoken(refreshToken: string, authorization: string) {
+    try {
+      const token = authorization.split(' ')[1]
+      const data = this.jwtService.verify(refreshToken);
       const user = await this.userRepository.findOne({
         where: {
-            username: logindata.username,
+          username: data.username,
         },
-        relations: [ 'roles']
-    });
-    if(!user){
-      throw new HttpException('用户名或者密码错误',HttpStatus.BAD_REQUEST)
-    }
-    if(md5(logindata.password)!==user.password){
-      throw new HttpException('用户名或者密码错误',HttpStatus.BAD_REQUEST)
-    }
-
-      const vo=new LoginVo();
-      vo.access_token = this.jwtService.sign({...user}, {
-        expiresIn: this.configService.get('jwt.expireIn') || '30m'
-      });
-      vo.refresh_token = this.jwtService.sign({...user}, {
-        expiresIn: this.configService.get('jwt.refreshExpire') || '7d'
-      });
-      const access_token_sessionid=vo.access_token.slice(-10)
-      const refresh_token_sessionid=vo.refresh_token.slice(-10)
-      this.redisService.setToken(`refreshToken:${refresh_token_sessionid}`,vo.refresh_token)
-      this.redisService.setToken(`accessToken:${access_token_sessionid}`,vo.access_token)
-     return vo
-    }
-
-    async refreshtoken(refreshToken:string, authorization:string){
-      try{
-        const token=authorization.split(' ')[1]
-        const data = this.jwtService.verify(refreshToken);
-        const user = await this.userRepository.findOne({
-          where: {
-              username:data.username,
-          },
-          relations: [ 'roles', 'roles.menus']
-        })
-        if(!user){
-          throw new UnauthorizedException('token 已失效，请重新登录');
-        }
-        try {
-          // 尝试验证token，这里期望可能抛出TokenExpiredError
-          const data = this.jwtService.verify(token);
-          throw new HttpException('token已刷新请勿重复刷新',HttpStatus.BAD_REQUEST)
-          // 如果没有错误抛出，表示token完全有效
+        relations: ['roles', 'roles.menus']
+      })
+      if (!user) {
+        throw new UnauthorizedException('token 已失效，请重新登录');
+      }
+      try {
+        // 尝试验证token，这里期望可能抛出TokenExpiredError
+        const data = this.jwtService.verify(token);
+        throw new HttpException('token已刷新请勿重复刷新', HttpStatus.BAD_REQUEST)
+        // 如果没有错误抛出，表示token完全有效
       } catch (error) {
-          if (error instanceof TokenExpiredError) {
-              // 如果捕获到TokenExpiredError，说明token有效但过期
-              // 在这里处理过期的token
-              const access_token_sessionid_history=token.slice(-10)
-              this.redisService.delToken(`accessToken:${access_token_sessionid_history}`)
-              const vo=new LoginVo();
-              vo.access_token = this.jwtService.sign({...user}, {
-                expiresIn: this.configService.get('jwt.expireIn') || '30m'
-              });
-              const access_token_sessionid=vo.access_token.slice(-10)
-              this.redisService.setToken(`accessToken:${access_token_sessionid}`,vo.access_token)
-              return vo
-              // 还可以继续执行需要的业务逻辑
-          } else {
-              // 如果抛出的不是TokenExpiredError，那么token可能在其他方面无效
-              if (error instanceof HttpException) {
-                // 重新抛出HttpException，或者根据需要处理它
-                throw error;
-              } else {
-                // 处理其他类型的错误
-                throw new UnauthorizedException('token 已失效，请重新登录');
-              }
-          }
-      }
-      }catch(e){
-        if (e instanceof HttpException) {
-          // 重新抛出HttpException，或者根据需要处理它
-          throw e;
+        if (error instanceof TokenExpiredError) {
+          // 如果捕获到TokenExpiredError，说明token有效但过期
+          // 在这里处理过期的token
+          const access_token_sessionid_history = token.slice(-10)
+          this.redisService.delToken(`accessToken:${access_token_sessionid_history}`)
+          const vo = new LoginVo();
+          vo.access_token = this.jwtService.sign({ ...user }, {
+            expiresIn: this.configService.get('jwt.expireIn') || '30m'
+          });
+          const access_token_sessionid = vo.access_token.slice(-10)
+          this.redisService.setToken(`accessToken:${access_token_sessionid}`, vo.access_token)
+          return vo
+          // 还可以继续执行需要的业务逻辑
         } else {
-          // 处理其他类型的错误
-          throw new UnauthorizedException('token 已失效，请重新登录');
+          // 如果抛出的不是TokenExpiredError，那么token可能在其他方面无效
+          if (error instanceof HttpException) {
+            // 重新抛出HttpException，或者根据需要处理它
+            throw error;
+          } else {
+            // 处理其他类型的错误
+            throw new UnauthorizedException('token 已失效，请重新登录');
+          }
         }
       }
-    }
-
-    async logout(refreshToken:string,authorization:string){
-      if(!refreshToken){
-        throw new HttpException('refreshToken不能为空',HttpStatus.BAD_REQUEST)
-      }
-      try{
-       this.jwtService.verify(refreshToken)
-       const access_token=authorization.split(' ')[1]
-       const access_token_sessionid=access_token.slice(-10)
-       const refresh_token_sessionid=refreshToken.slice(-10)
-       this.redisService.delToken(`accessToken:${access_token_sessionid}`)
-       this.redisService.delToken(`refreshToken:${refresh_token_sessionid}`)
-       return '退出成功'
-      }catch(error){
+    } catch (e) {
+      if (e instanceof HttpException) {
+        // 重新抛出HttpException，或者根据需要处理它
+        throw e;
+      } else {
+        // 处理其他类型的错误
         throw new UnauthorizedException('token 已失效，请重新登录');
       }
     }
   }
+
+  async logout(refreshToken: string, authorization: string) {
+    if (!refreshToken) {
+      throw new HttpException('refreshToken不能为空', HttpStatus.BAD_REQUEST)
+    }
+    try {
+      this.jwtService.verify(refreshToken)
+      const access_token = authorization.split(' ')[1]
+      const access_token_sessionid = access_token.slice(-10)
+      const refresh_token_sessionid = refreshToken.slice(-10)
+      this.redisService.delToken(`accessToken:${access_token_sessionid}`)
+      this.redisService.delToken(`refreshToken:${refresh_token_sessionid}`)
+      return '退出成功'
+    } catch (error) {
+      throw new UnauthorizedException('token 已失效，请重新登录');
+    }
+  }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,199 +1,199 @@
-import { Injectable ,HttpException,HttpStatus} from '@nestjs/common';
+import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import {  In, Repository,Like } from 'typeorm';
-import {User} from '@/user/entities/user.entity';
-import {Role} from '@/role/entities/role.entity';
-import {Menu} from '@/menu/entities/menu.entity';
+import { In, Repository, Like } from 'typeorm';
+import { User } from '@/user/entities/user.entity';
+import { Role } from '@/role/entities/role.entity';
+import { Menu } from '@/menu/entities/menu.entity';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import {PasswordDto} from './dto/password.dto';
+import { PasswordDto } from './dto/password.dto';
 import { StatusDto } from './dto/status.dto';
 import { QueryDto } from './dto/query.dto';
-import {PageResponseVo} from './vo/page-response.vo';
+import { PageResponseVo } from './vo/page-response.vo';
 import { md5 } from '@/utils';
 import {
   paginate,
   IPaginationOptions,
 } from 'nestjs-typeorm-paginate';
-import {IResponseData,IResponsePagerData} from '@/utils/types';
-import {formatResponsePagerData} from '@/utils/index';
+import { IResponseData, IResponsePagerData } from '@/utils/types';
+import { formatResponsePagerData } from '@/utils/index';
 
 @Injectable()
 export class UserService {
 
-@InjectRepository(User)
-private userRepository: Repository<User>;
+  @InjectRepository(User)
+  private userRepository: Repository<User>;
 
-@InjectRepository(Role)
-private roleRepository: Repository<Role>;
+  @InjectRepository(Role)
+  private roleRepository: Repository<Role>;
 
-@InjectRepository(Menu)
-private menuRepository: Repository<Menu>;
+  @InjectRepository(Menu)
+  private menuRepository: Repository<Menu>;
 
-async create(createUserDto: CreateUserDto) {
-  try{   
-    const user = new User();
-    user.username=createUserDto.userName
-    user.nickName=createUserDto.nickName
-    user.accountStatus=createUserDto.accountStatus
-    user.password=md5(createUserDto.password)
-    user.email=createUserDto.email
-    user.mobile=createUserDto.mobile
-    user.remark=createUserDto.remark
-    const roles=await this.roleRepository.findBy({
-      id:In(createUserDto.roleIds)
-    })
-    user.roles=roles
-    await this.userRepository.save(user)
-    return '创建用户成功'
-  }catch(error){
-    throw new HttpException(error,HttpStatus.INTERNAL_SERVER_ERROR)
-  }
-}
-
-async update(updateUserDto: UpdateUserDto) {
-    try{
-      const user=await this.userRepository.findOneBy({id:updateUserDto.id})
-      if(!user){
-        throw new HttpException('用户不存在',HttpStatus.BAD_REQUEST)
-      }
-      user.username=updateUserDto.userName
-      user.nickName=updateUserDto.nickName
-      user.accountStatus=updateUserDto.accountStatus
-      user.email=updateUserDto.email
-      user.mobile=updateUserDto.mobile
-      user.remark=updateUserDto.remark
-      const roles=await this.roleRepository.findBy({
-        id:In(updateUserDto.roleIds)
+  async create(createUserDto: CreateUserDto) {
+    try {
+      const user = new User();
+      user.username = createUserDto.username
+      user.nickName = createUserDto.nickName
+      user.accountStatus = createUserDto.accountStatus
+      user.password = md5(createUserDto.password)
+      user.email = createUserDto.email
+      user.mobile = createUserDto.mobile
+      user.remark = createUserDto.remark
+      const roles = await this.roleRepository.findBy({
+        id: In(createUserDto.roleIds)
       })
-      user.roles=roles
+      user.roles = roles
+      await this.userRepository.save(user)
+      return '创建用户成功'
+    } catch (error) {
+      throw new HttpException(error, HttpStatus.INTERNAL_SERVER_ERROR)
+    }
+  }
+
+  async update(updateUserDto: UpdateUserDto) {
+    try {
+      const user = await this.userRepository.findOneBy({ id: updateUserDto.id })
+      if (!user) {
+        throw new HttpException('用户不存在', HttpStatus.BAD_REQUEST)
+      }
+      user.username = updateUserDto.username
+      user.nickName = updateUserDto.nickName
+      user.accountStatus = updateUserDto.accountStatus
+      user.email = updateUserDto.email
+      user.mobile = updateUserDto.mobile
+      user.remark = updateUserDto.remark
+      const roles = await this.roleRepository.findBy({
+        id: In(updateUserDto.roleIds)
+      })
+      user.roles = roles
       await this.userRepository.save(user)
       return '更新用户成功'
-    }catch(error){
-      if(error instanceof HttpException){
+    } catch (error) {
+      if (error instanceof HttpException) {
         throw error
       }
-      throw new HttpException(error,HttpStatus.INTERNAL_SERVER_ERROR)
+      throw new HttpException(error, HttpStatus.INTERNAL_SERVER_ERROR)
     }
-}
+  }
 
-async updatePassword(passwordDto: PasswordDto){
-      try{
-      const {userId,newPassword,repPassword}=passwordDto
-      const user=await this.userRepository.findOneBy({id:userId})
-      if(!user){
-        throw new HttpException('用户不存在',HttpStatus.BAD_REQUEST)
+  async updatePassword(passwordDto: PasswordDto) {
+    try {
+      const { userId, newPassword, repPassword } = passwordDto
+      const user = await this.userRepository.findOneBy({ id: userId })
+      if (!user) {
+        throw new HttpException('用户不存在', HttpStatus.BAD_REQUEST)
       }
-      if(newPassword!==repPassword){
-        throw new HttpException('两次密码不一致',HttpStatus.BAD_REQUEST)
+      if (newPassword !== repPassword) {
+        throw new HttpException('两次密码不一致', HttpStatus.BAD_REQUEST)
       }
-      user.password=md5(newPassword)
+      user.password = md5(newPassword)
       await this.userRepository.save(user)
       return '重置密码成功'
-      }catch(error){
-        if(error instanceof HttpException){
-          throw error
-        }
-        throw new HttpException(error,HttpStatus.INTERNAL_SERVER_ERROR)
-      }
-}
-
-async updateStatus(id:number, statusDto:StatusDto){
-    try{
-      const {userId,accountStatus}=statusDto
-      const user=await this.userRepository.findOneBy({id:userId})
-      if(!user){
-        throw new HttpException('用户不存在',HttpStatus.BAD_REQUEST)
-      }
-      if(userId===id){
-        throw new HttpException('不能禁用自己',HttpStatus.BAD_REQUEST)
-      }
-      user.accountStatus=accountStatus
-      await this.userRepository.save(user)
-      return '更新状态成功'
-    }catch(error){
-      if(error instanceof HttpException){
+    } catch (error) {
+      if (error instanceof HttpException) {
         throw error
       }
-      throw new HttpException(error,HttpStatus.INTERNAL_SERVER_ERROR)
+      throw new HttpException(error, HttpStatus.INTERNAL_SERVER_ERROR)
     }
-}
-
-async remove(id: number) {
-  try{
-    const user=await this.userRepository.findOneBy({id:id})
-    if(!user){
-      throw new HttpException('用户不存在',HttpStatus.BAD_REQUEST)
-    }
-    await this.userRepository.remove(user)
-    return '删除用户成功'
-  }catch(error){
-    throw new HttpException(error,HttpStatus.INTERNAL_SERVER_ERROR)
   }
-}
 
-async findMany(query:QueryDto) :Promise<IResponseData<PageResponseVo>>{
-  try{
-   const {current:page,size:limit,keyword=''}=query
-    const options: IPaginationOptions = {
-      page: page,
-      limit: limit,
+  async updateStatus(id: number, statusDto: StatusDto) {
+    try {
+      const { userId, accountStatus } = statusDto
+      const user = await this.userRepository.findOneBy({ id: userId })
+      if (!user) {
+        throw new HttpException('用户不存在', HttpStatus.BAD_REQUEST)
+      }
+      if (userId === id) {
+        throw new HttpException('不能禁用自己', HttpStatus.BAD_REQUEST)
+      }
+      user.accountStatus = accountStatus
+      await this.userRepository.save(user)
+      return '更新状态成功'
+    } catch (error) {
+      if (error instanceof HttpException) {
+        throw error
+      }
+      throw new HttpException(error, HttpStatus.INTERNAL_SERVER_ERROR)
     }
-    const searchOptions = {
-      where: [
-        { username: Like(`%${keyword}%`) },
-        { email: Like(`%${keyword}%`) },
-        { mobile: Like(`%${keyword}%`) },
-      ],
-      relations: ['roles']  // 在这里添加角色关系
-    };
-    const users:IResponsePagerData<PageResponseVo>= await paginate<User>(this.userRepository, options, searchOptions);
-    users.items.forEach((user:PageResponseVo)=>{
-      user.roleIds= user.roles.map(role=>role.id)
-      delete user.roles
-      delete user.password
-    })
-    return  formatResponsePagerData<PageResponseVo>(users)
-  }catch(error){
-    throw new HttpException(error,HttpStatus.INTERNAL_SERVER_ERROR)
   }
-}
 
-async findAll(){
-  try{
-    const users:PageResponseVo[]=await this.userRepository.find()
-    users.forEach(item=>delete item.password)
-    return users
-  }catch(error){
-    throw new HttpException(error,HttpStatus.INTERNAL_SERVER_ERROR)
+  async remove(id: number) {
+    try {
+      const user = await this.userRepository.findOneBy({ id: id })
+      if (!user) {
+        throw new HttpException('用户不存在', HttpStatus.BAD_REQUEST)
+      }
+      await this.userRepository.remove(user)
+      return '删除用户成功'
+    } catch (error) {
+      throw new HttpException(error, HttpStatus.INTERNAL_SERVER_ERROR)
+    }
   }
-}
 
-async exist(mobile?:string,email?:string,username?:string){
-  try{
-    const params=[mobile,email,username].filter(x=>x!==undefined)
-    if(params.length!==1){
-      throw new HttpException('传参错误',HttpStatus.BAD_REQUEST)
+  async findMany(query: QueryDto): Promise<IResponseData<PageResponseVo>> {
+    try {
+      const { current: page, size: limit, keyword = '' } = query
+      const options: IPaginationOptions = {
+        page: page,
+        limit: limit,
+      }
+      const searchOptions = {
+        where: [
+          { username: Like(`%${keyword}%`) },
+          { email: Like(`%${keyword}%`) },
+          { mobile: Like(`%${keyword}%`) },
+        ],
+        relations: ['roles']  // 在这里添加角色关系
+      };
+      const users: IResponsePagerData<PageResponseVo> = await paginate<User>(this.userRepository, options, searchOptions);
+      users.items.forEach((user: PageResponseVo) => {
+        user.roleIds = user.roles.map(role => role.id)
+        delete user.roles
+        delete user.password
+      })
+      return formatResponsePagerData<PageResponseVo>(users)
+    } catch (error) {
+      throw new HttpException(error, HttpStatus.INTERNAL_SERVER_ERROR)
     }
-    if(mobile){
-      const user=await this.userRepository.findOneBy({mobile:mobile})
-      return user?false:true
-    }
-    if(email){
-      const user=await this.userRepository.findOneBy({email:email})
-      return user?false:true
-    }
-    if(username){
-      const user=await this.userRepository.findOneBy({username:username})
-      return user?false:true
-    }
-  }catch(error){
-    if(error instanceof HttpException){
-      throw error
-    }
-    throw new HttpException(error,HttpStatus.INTERNAL_SERVER_ERROR)
   }
-}
+
+  async findAll() {
+    try {
+      const users: PageResponseVo[] = await this.userRepository.find()
+      users.forEach(item => delete item.password)
+      return users
+    } catch (error) {
+      throw new HttpException(error, HttpStatus.INTERNAL_SERVER_ERROR)
+    }
+  }
+
+  async exist(mobile?: string, email?: string, username?: string) {
+    try {
+      const params = [mobile, email, username].filter(x => x !== undefined)
+      if (params.length !== 1) {
+        throw new HttpException('传参错误', HttpStatus.BAD_REQUEST)
+      }
+      if (mobile) {
+        const user = await this.userRepository.findOneBy({ mobile: mobile })
+        return user ? true : false
+      }
+      if (email) {
+        const user = await this.userRepository.findOneBy({ email: email })
+        return user ? true : false
+      }
+      if (username) {
+        const user = await this.userRepository.findOneBy({ username: username })
+        return user ? true : false
+      }
+    } catch (error) {
+      if (error instanceof HttpException) {
+        throw error
+      }
+      throw new HttpException(error, HttpStatus.INTERNAL_SERVER_ERROR)
+    }
+  }
 
   findOne(id: number) {
     return `This action returns a #${id} user`;
